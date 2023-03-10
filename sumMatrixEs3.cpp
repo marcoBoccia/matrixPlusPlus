0c4d8955ad0380fe94db71fc9475f05de310d8fe
Marco Boccia <pokermark95@live.it>
2023-03-10 15:51:20 +0100

    risolto problema spazi es3

--- sumMatrixEs3.cpp
+++ sumMatrixEs3.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib> // per rand() e srand()
+#include <ctime> // per time()
 
 using namespace std;
 
@@ -8,37 +11,39 @@ int main()
     const int COLS = 4;
     int matrix[ROWS][COLS];
 
-    // Leggi la matrice da input
-    cout << "Inserisci i valori della matrice 4x4:" << endl;
+    // Imposta il seme del generatore di numeri casuali
+    srand(time(NULL));
+
+    // Popola la matrice con numeri casuali
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            cin >> matrix[i][j];
+            matrix[i][j] = rand() % 100; // genera numeri casuali tra 0 e 99
         }
     }
 
-    // Stampa la matrice originale con la somma di ogni riga e colonna
+    // Stampa la matrice con la somma di ogni riga e colonna
     cout << "Matrice con somma di ogni riga e colonna:" << endl;
     for (int i = 0; i < ROWS; i++) {
         int row_sum = 0;
         for (int j = 0; j < COLS; j++) {
-            cout << matrix[i][j] << " ";
+            cout << setw(4) << matrix[i][j] << " ";
             row_sum += matrix[i][j];
         }
-        cout << "| " << row_sum << endl;
+        cout << "| " << setw(4) << row_sum << endl;
     }
 
     // Stampa la somma di ogni colonna
-    cout << " ";
+    cout << setw(5) << " ";
     for (int j = 0; j < COLS; j++) {
         cout << "----";
     }
-    cout << "----" << endl;
+    cout << "-----" << endl;
     for (int j = 0; j < COLS; j++) {
         int col_sum = 0;
         for (int i = 0; i < ROWS; i++) {
             col_sum += matrix[i][j];
         }
-        cout << col_sum << " ";
+        cout << setw(4) << col_sum << " ";
     }
     cout << "| ";
     int total_sum = 0;
@@ -47,7 +52,7 @@ int main()
             total_sum += matrix[i][j];
         }
     }
-    cout << total_sum << endl;
+    cout << setw(4) << total_sum << endl;
 
     return 0;
 }
